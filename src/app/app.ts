8059954e3acc2966d07479804ac674dcb504d263
Leonardo Maglio <leotagit@gmail.com>
2019-10-06 21:51:37 +0200

    Material improvements with envMap and mat tuning

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer, Math, HemisphereLight } from 'three';
+import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer, Math, HemisphereLight, BackSide, Mesh } from 'three';
 import { Pyramid } from './pyramid';
 import { TranslucentMaterial } from './materials';
 
@@ -13,20 +13,33 @@ export class App {
     private pyramid: Pyramid;
 
     constructor() {
-        this.pyramid = new Pyramid(20, 25, new TranslucentMaterial());
-        this.pyramid.rotateY(Math.degToRad(45));
-        this.scene.add(this.pyramid);
-        
-        this.camera.position.set(0, 0, 200);
+        this.camera.position.set(0, 0, 100);
         this.camera.lookAt(new Vector3(0, 0, 0));
         
         this.renderer.setSize(innerWidth, innerHeight);
         this.renderer.setClearColor(new Color('rgb(2,16,76)'));
 
+        // 3D objects
+        this.pyramid = this.createPyramid();
+        this.scene.add(this.pyramid);
+
+        // lights
         const light = new HemisphereLight(0xffffbb, 0x080820, 1);
         this.scene.add(light);
         
         this.render();
+    }
+
+    private createPyramid(): Mesh {
+        const innreMat = new TranslucentMaterial();
+        innreMat.side = BackSide;
+        const innrePyramid = new Pyramid(18, 23, innreMat);
+
+        const outerPyramid = new Pyramid(20, 25, new TranslucentMaterial());
+        outerPyramid.add(innrePyramid);
+        outerPyramid.rotateY(Math.degToRad(45));
+
+        return outerPyramid;
     }
     
     private adjustCanvasSize(): void {
@@ -40,7 +53,7 @@ export class App {
         requestAnimationFrame(() => this.render());
         
         this.adjustCanvasSize();
-        this.pyramid.rotateZ(0.01);
+        this.pyramid.rotateZ(0.005);
         this.pyramid.rotateY(0.03);
     }
 }
--- src/app/materials.ts
+++ src/app/materials.ts
@@ -1,18 +1,27 @@
-import { MeshPhysicalMaterial, MeshPhysicalMaterialParameters } from "three";
+import { CubeTextureLoader, MeshPhongMaterial, MeshPhongMaterialParameters } from "three";
 
-const MATERIAL_DEFAULT_PARAMS: MeshPhysicalMaterialParameters = {
+const loader = new CubeTextureLoader();
+loader.setPath('src/assets/');
+
+const cubeMap = loader.load([
+    'px.png', 'nx.png',
+    'py.png', 'ny.png',
+    'pz.png', 'nz.png'
+]);
+
+const MATERIAL_DEFAULT_PARAMS: MeshPhongMaterialParameters = {
     transparent: true,
     opacity: 0.8,
     color: 0xff0000,
     emissive: 0xb74c4c,
-    roughness: 0.2,
-    metalness: 0.75,
-    reflectivity: 0.35,
-    clearcoat: 1,
-    clearcoatRoughness: 0.45
+    emissiveIntensity: 1,
+    specular: 0xfff,
+    shininess: 1,
+    reflectivity: 1,
+    envMap: cubeMap,
 }
 
-export class TranslucentMaterial extends MeshPhysicalMaterial {
+export class TranslucentMaterial extends MeshPhongMaterial {
     constructor() {
         super(MATERIAL_DEFAULT_PARAMS);
     }
